a23b9348e37a0438b835d80fc62e4a94721d54c7
Kevin Jones <kevin.jonesat@gmail.com>
2019-08-28 14:24:37 +0100

    Add script to build babble core

---
+++ babble/libs/bbc.sh
@@ -0,0 +1,22 @@
+# build babble core (bbc)
+set -e
+
+babble_core_version=0.5.1
+
+if [ -d build ]; then rm -r build; fi
+mkdir build
+cd build
+
+if [ -z ${GOPATH+x} ]; then 
+    # GOPATH is not set, so we'll use the standard Go tree
+    cp ~/go/src/github.com/mosaicnetworks/babble/src/mobile/build/${babble_core_version}/mobile.aar .
+else 
+    cp ${GOPATH}/github.com/mosaicnetworks/babble/src/mobile/build/${babble_core_version}/mobile.aar .
+fi
+
+unzip mobile.aar -d mobile.aar.unzip
+cd ..
+cp build/mobile.aar.unzip/classes.jar .
+cp -r build/mobile.aar.unzip/jni .
+
+rm -r build
