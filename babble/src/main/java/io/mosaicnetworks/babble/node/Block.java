692af78a76717047615579ead54ac4444297bf34
Kevin Jones <kevin.jonesat@gmail.com>
2019-10-16 13:31:54 +0100

    Updated block class
    Created a fromJson factory and made class immutable

--- babble/src/main/java/io/mosaicnetworks/babble/node/Block.java
+++ babble/src/main/java/io/mosaicnetworks/babble/node/Block.java
@@ -1,30 +1,54 @@
 package io.mosaicnetworks.babble.node;
 
+import android.util.Base64;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
 
-import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.Map;
 
+class Block {
 
-class Block implements Serializable {
+    private final static Gson mCustomGson;
 
-    public class BlockBody implements Serializable {
+    static {
+        mCustomGson = new GsonBuilder().registerTypeAdapter(byte[].class, new
+                JsonDeserializer<byte[]>() {public byte[] deserialize(JsonElement json, Type
+                        typeOfT, JsonDeserializationContext context) throws JsonSyntaxException {
+                    return Base64.decode(json.getAsString(), Base64.NO_WRAP);
+                }
+                }).create();
+    }
+
+    public static final class BlockBody {
         @SerializedName("Index")
-        public int index;
+        public final int index = 0;
 
         @SerializedName("RoundReceived")
-        public int roundReceived;
+        public final int roundReceived = 0;
 
         @SerializedName("StateHash")
-        public byte[] stateHash;
+        public final byte[] stateHash = new byte[]{};
 
         @SerializedName("Transactions")
-        public byte[][] transactions;
+        public final byte[][] transactions = new byte[][]{};
+
     }
 
     @SerializedName("Body")
-    public BlockBody body;
+    public final BlockBody body = new BlockBody();
 
     @SerializedName("Signatures")
-    public Map<String, String> signatures;
+    public final Map<String, String> signatures = null;
+
+    public static Block fromJson(String blockJson) {
+        //TODO: implement checks for correct types and missing and extra JSON attributes using GSON
+        return mCustomGson.fromJson(blockJson, Block.class);
+    }
 }
