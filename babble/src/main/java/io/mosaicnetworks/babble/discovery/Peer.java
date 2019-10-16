318d5ca6b56a514746da332c3f53a58dfdef3ad6
Kevin Jones <kevin.jonesat@gmail.com>
2019-10-16 14:58:06 +0100

    Made Peer class immutable

--- babble/src/main/java/io/mosaicnetworks/babble/discovery/Peer.java
+++ babble/src/main/java/io/mosaicnetworks/babble/discovery/Peer.java
@@ -2,18 +2,23 @@ package io.mosaicnetworks.babble.discovery;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Peer {
+public final class Peer {
 
     @SerializedName("PubKeyHex")
-    public String pubKeyHex;
+    public final String pubKeyHex;
 
     @SerializedName("NetAddr")
-    public String netAddr;
+    public final String netAddr;
 
     @SerializedName("Moniker")
-    public String moniker;
+    public final String moniker;
 
     public Peer(String pubKeyHex, String netAddr, String moniker) {
+
+        if (pubKeyHex==null || netAddr==null || moniker==null) {
+            throw new NullPointerException("Null arguments are not accepted");
+        }
+
         this.pubKeyHex = pubKeyHex;
         this.netAddr = netAddr;
         this.moniker = moniker;
