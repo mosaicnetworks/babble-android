c95278ec70d4f428fb874ddb9e81a268e4eeb056
Kevin Jones <kevin.jonesat@gmail.com>
2019-11-04 15:35:05 +0000

    Added discovery to messaging service

--- sample/src/main/java/io/mosaicnetworks/sample/MessagingService.java
+++ sample/src/main/java/io/mosaicnetworks/sample/MessagingService.java
@@ -1,13 +1,13 @@
 package io.mosaicnetworks.sample;
 
-import android.content.Context;
-
 import com.google.gson.JsonSyntaxException;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.mosaicnetworks.babble.discovery.HttpPeerDiscoveryServer;
 import io.mosaicnetworks.babble.discovery.Peer;
 import io.mosaicnetworks.babble.node.BabbleNode;
 import io.mosaicnetworks.babble.node.BabbleNodeListeners;
@@ -16,17 +16,20 @@ import io.mosaicnetworks.babble.node.LeaveResponseListener;
 
 public class MessagingService {
 
-    private enum State {
+    public enum State {
         UNCONFIGURED,
         CONFIGURED,
-        RUNNING
+        RUNNING,
+        RUNNING_WITH_DISCOVERY
     }
 
     private static MessagingService instance;
     private List<MessageObserver> mObservers = new ArrayList<>();
     private BabbleNode mBabbleNode;
+    private HttpPeerDiscoveryServer mHttpPeerDiscoveryServer;
     private KeyPair mKeyPair = new KeyPair();
     private static final int BABBLING_PORT = 6666;
+    private static final int DISCOVERY_PORT = 8988;
     private byte[] mStateHash;
     private State mState = State.UNCONFIGURED;
 
@@ -39,7 +42,7 @@ public class MessagingService {
 
     public void configure(List<Peer> peers, String moniker, String inetAddress) {
 
-        if (mState==State.RUNNING) {
+        if (mState==State.RUNNING || mState==State.RUNNING_WITH_DISCOVERY) {
             throw new IllegalStateException("Cannot configure while the service is running");
         }
 
@@ -66,29 +69,43 @@ public class MessagingService {
                                 notifyObservers(message);
                             }
 
+                            //TODO: update state hash
                             return mStateHash;
                         }
                     });
             mState = State.CONFIGURED;
         } catch (IllegalArgumentException ex) {
-            //The reassignment of mState has failed, so leave it and the service state as before
+            //The reassignment of mState and MBabbleNode has failed, so leave them as before
             //TODO: need to catch port in use exception (IOException) and throw others
         }
+
+        mHttpPeerDiscoveryServer = new HttpPeerDiscoveryServer(inetAddress, DISCOVERY_PORT, mBabbleNode);
     }
 
     public void start() {
-        if (mState==State.UNCONFIGURED || mState==State.RUNNING) {
+        if (mState==State.UNCONFIGURED || mState==State.RUNNING ||
+                mState==State.RUNNING_WITH_DISCOVERY) {
             throw new IllegalStateException("Cannot start an unconfigured or running service");
         }
 
         mBabbleNode.run();
         mState=State.RUNNING;
+
+        try {
+            mHttpPeerDiscoveryServer.start();
+            mState=State.RUNNING_WITH_DISCOVERY;
+        } catch (IOException ex) {
+            //Probably the port is in use, we'll continue without the discovery service
+        }
     }
 
     public void stop() {
-        if (mState!=State.RUNNING) {
+        if (!(mState==State.RUNNING || mState==State.RUNNING_WITH_DISCOVERY)) {
             throw new IllegalStateException("Cannot stop a service which isn't running");
         }
+
+        mHttpPeerDiscoveryServer.stop();
+        mHttpPeerDiscoveryServer=null;
 
         mBabbleNode.leave(new LeaveResponseListener() {
             @Override
@@ -100,7 +117,14 @@ public class MessagingService {
     }
 
     public void submitMessage(Message message) {
+        if (!(mState==State.RUNNING || mState==State.RUNNING_WITH_DISCOVERY)) {
+            throw new IllegalStateException("Cannot submit when the service isn't running");
+        }
         mBabbleNode.submitTx(message.toBabbleTx().toBytes());
+    }
+
+    public State getState() {
+        return mState;
     }
 
     public void registerObserver(MessageObserver messageObserver) {
