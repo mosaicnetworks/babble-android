f9165a929782979e1ab224d5bcf1c9fe878fa4ef
Kevin Jones <kevin.jonesat@gmail.com>
2019-11-28 13:41:05 +0000

    Added MessaginService and AppState tests

--- sample/src/test/java/io/mosaicnetworks/sample/AppStateTest.java
+++ sample/src/test/java/io/mosaicnetworks/sample/AppStateTest.java
@@ -4,10 +4,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import javax.xml.bind.DatatypeConverter;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AppStateTest {
