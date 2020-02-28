b856258eebb846f2ceffdd04daacf494eab677aa
Jon Knight <jon@babble.io>
2020-02-28 14:54:24 +0000

    Multicolour avatars

--- sample/src/main/java/io/mosaicnetworks/sample/ChatActivity.java
+++ sample/src/main/java/io/mosaicnetworks/sample/ChatActivity.java
@@ -109,6 +109,7 @@ public class ChatActivity extends AppCompatActivity implements ServiceObserver {
                 }
             }
         });
+
         mMessagesList.setAdapter(mAdapter);
 
         MessageInput input = findViewById(R.id.input);
